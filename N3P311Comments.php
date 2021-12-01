eaf7e7233d4609f48e034383138fb43116fb9dff
daw2-aspas21 <71894719+daw2-aspas21@users.noreply.github.com>
2021-12-01 16:46:54 +0100

    Add files via upload

---
+++ N3P311Comments.php
@@ -0,0 +1,19 @@
+<ul>
+    <li>How to link a stars with a form in each document</li>
+    <li>How to link differents form into differents pages</li>
+    <li>Create a form with several input boxes and connect them with other form</li>
+    <li>How to sum different numbers into the same form</li>
+    <li>How to use a echo</li>
+    <li>How to use a input type</li>
+    <li>How to use a link</li>
+    <li>How to insert new reviews for each movie</li>
+    <li>How to put an AVG function into each movie</li>
+    <li>How to colocate the movies into a certain order to low to top</li>
+</ul>
+
+<p>Document: 6</p>
+        <p>Teacher's explain: 8</p>
+        <p>MySelf Mark: 7</p>
+        <p>How create a order by column for the third exercise</p>
+  </body>
+</html>
